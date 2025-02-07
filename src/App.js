b0244dae3ea6483a2a45d3623d0acbda40e6006c
sahuji90 <sahusunil90@gmail.com>
2025-02-07 23:18:47 +0530

    using react state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ToDoTable from './components/ToDoTable';
 function App() {
-  const todos = [{rownum:'1',taskName:'Study React',taskAssigned:'user1'},
-                 {rownum:'2',taskName:'Go To Gym',taskAssigned:'user1'}, 
-                 {rownum:'3',taskName:'Study Angular',taskAssigned:'user2'},
-                 {rownum:'4',taskName:'Charge EV',taskAssigned:'user2'}];
+const [todos,setTodos] = useState(
+    [{rownum:'1',taskName:'Study React',taskAssigned:'user1'},
+    {rownum:'2',taskName:'Go To Gym',taskAssigned:'user1'}, 
+    {rownum:'3',taskName:'Study Angular',taskAssigned:'user2'},
+    {rownum:'4',taskName:'Charge EV',taskAssigned:'user2'}]
+  ) 
   const addTodo = () => {
     if(todos.length > 0){
-      const newTodo = {rownum: 1, taskName: 'New Task', taskAssigned: 'New User'};
-      todos.push(newTodo);
-      console.log(todos);
+      const newTodo = {rownum: todos.length+1, taskName: 'New Task', taskAssigned: 'New User'};
+      setTodos([...todos,newTodo]);
     } 
   };
   return (
