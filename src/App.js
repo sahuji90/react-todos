c08a722d3b8acb257ba6372657c37decf2a3dbc0
sahuji90 <sahusunil90@gmail.com>
2025-02-07 22:58:58 +0530

    added onClick event listeners

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
                  {rownum:'2',taskName:'Go To Gym',taskAssigned:'user1'}, 
                  {rownum:'3',taskName:'Study Angular',taskAssigned:'user2'},
                  {rownum:'4',taskName:'Charge EV',taskAssigned:'user2'}];
+  const addTodo = () => {
+    if(todos.length > 0){
+      const newTodo = {rownum: 1, taskName: 'New Task', taskAssigned: 'New User'};
+      todos.push(newTodo);
+      console.log(todos);
+    } 
+  };
   return (
     <div className='mt-5 container'>
         <div className="card">
@@ -14,6 +21,7 @@ function App() {
           </div>
           <div className="card-body">
             <ToDoTable todos={todos}/>
+            <button class='btn btn-primary' onClick={addTodo}>Add new Todo</button>
           </div>
         </div>
         
